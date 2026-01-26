337ed1d6f4d8e2c0a7da7ef440d9eb5523985d86
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2026-01-26 20:48:28 +0700

    Update Heading.vue

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import { Undo2 } from 'lucide-vue-next';
+import Button from './ui/button/Button.vue';
+
 interface Props {
     title: string;
     description?: string;
+    backUrl?: string;
 }
 
 defineProps<Props>();
@@ -9,7 +14,14 @@ defineProps<Props>();
 
 <template>
     <div class="mb-8 space-y-0.5">
-        <h2 class="text-xl font-semibold tracking-tight">{{ title }}</h2>
+        <div class="flex items-center gap-2">
+            <Link v-if="backUrl" :href="backUrl">
+                <Button>
+                    <Undo2 class="h-5 w-5" />
+                </Button>
+            </Link>
+            <h2 class="text-xl font-semibold tracking-tight">{{ title }}</h2>
+        </div>
         <p v-if="description" class="text-sm text-muted-foreground">
             {{ description }}
         </p>
